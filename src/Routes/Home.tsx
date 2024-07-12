8784a14e7487393628cfaf8a60be6ea4e04b9338
lemonsaltytoo <v-lvkdl@naver.com>
2024-07-12 11:36:15 +0900

    Changed a loader in Home & TV part

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -29,12 +29,28 @@ import {
 // styled-components
 const Container = styled.div``;
 
-// ****** wf I can't find any loader........... ////
 const Loader = styled.div`
-  color: pink;
-  font-size: 50px;
-  position: fixed;
-  top: 100px;
+  position: absolute;
+  top: 50vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 80px;
+  padding: 13px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: white;
+  --_m: conic-gradient(#0000 10%, #000), linear-gradient(#000 0 0) content-box;
+  -webkit-mask: var(--_m);
+  mask: var(--_m);
+  -webkit-mask-composite: source-out;
+  mask-composite: subtract;
+  animation: l3 1s infinite linear;
+  @keyframes l3 {
+    to {
+      transform: rotate(1turn);
+    }
+  }
 `;
 
 const Banner = styled.div<{ $bgPhoto: string }>`
--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -26,7 +26,29 @@ import { useMatch, useNavigate } from "react-router-dom";
 
 const Container = styled.div``;
 
-const Loader = styled.div``;
+const Loader = styled.div`
+  position: absolute;
+  top: 50vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 80px;
+  padding: 13px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: white;
+  --_m: conic-gradient(#0000 10%, #000), linear-gradient(#000 0 0) content-box;
+  -webkit-mask: var(--_m);
+  mask: var(--_m);
+  -webkit-mask-composite: source-out;
+  mask-composite: subtract;
+  animation: l3 1s infinite linear;
+  @keyframes l3 {
+    to {
+      transform: rotate(1turn);
+    }
+  }
+`;
 
 const Banner = styled.div<{ $bgPhoto: string }>`
   width: 100%;
